8e5baaf953cef98da313bf1aa0bd9678dc5a6f97
Offuru <andrei.rozmarin@gmail.com>
2023-11-09 19:03:14 +0200

    Added graph and input listener fields

--- GraphBuilder/src/GraphBuilder/Panel.java
+++ GraphBuilder/src/GraphBuilder/Panel.java
@@ -8,14 +8,25 @@ import GraphBuilder.managers.NodeManager;
 import javax.swing.*;
 import java.awt.*;
 
+import GraphBuilder.models.Graph;
+import GraphBuilder.models.Node;
+import GraphBuilder.models.Edge;
+
 public class Panel extends JPanel {
 
-    public static Color backgroundColor = new Color(111, 103, 157);
+    private Graph graph;
+
+    public MouseListener mouseListener;
+    public KeyboardListener keyboardListener;
+
+
+    public static Color backgroundColor = new Color(140, 134, 180);
 
     public Panel(){
 
         setBackground(backgroundColor);
-
+        graph = new Graph();
+        mouseListener = new MouseListener(this);
 
     }
 
