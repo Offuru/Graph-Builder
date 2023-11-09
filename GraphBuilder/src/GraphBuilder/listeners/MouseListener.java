5f8a0d09bf1635b58bb096545d0778b93b350d49
Offuru <andrei.rozmarin@gmail.com>
2023-11-09 20:06:19 +0200

    Implemented node and edge adding and removal via mouse input

--- GraphBuilder/src/GraphBuilder/listeners/MouseListener.java
+++ GraphBuilder/src/GraphBuilder/listeners/MouseListener.java
@@ -14,7 +14,7 @@ public class MouseListener implements javax.swing.event.MouseInputListener {
     Node end;
     int draggedNodeIndex;
 
-    public MouseListener(Panel panel){
+    public MouseListener(Panel panel) {
 
         this.panel = panel;
         isDragging = false;
@@ -25,22 +25,76 @@ public class MouseListener implements javax.swing.event.MouseInputListener {
     }
 
     @Override
-    public void mouseDragged(MouseEvent e) {
+    public void mousePressed(MouseEvent e) {
 
+        if (start == null) {
+
+            for (Node node : panel.getGraph().getNodes())
+                if (node.containsPoint(e.getPoint())) {
+                    start = node;
+                    node.select();
+                    draggedNodeIndex = node.getKey();
+                    break;
+                }
+
+        } else {
+
+            for (Node node : panel.getGraph().getNodes())
+                if (node.containsPoint(e.getPoint())) {
+                    if (node == start) {
+                        start = null;
+                        node.unselect();
+                    } else {
+                        end = node;
+                    }
+
+                    break;
+                }
+        }
+
+        if (start != null && end != null) {
+
+            if (panel.getGraph().isEdge(start, end))
+                panel.getGraph().removeEdge(start, end);
+            else
+                panel.getGraph().addEdge(start, end);
+
+            start.unselect();
+            start = end = null;
+        }
+
+        panel.repaint();
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {
+    public void mouseReleased(MouseEvent e) {
+
+        if (start == null) {
+
+            boolean canPlace = true;
+
+            for (Node node : panel.getGraph().getNodes())
+                if (e.getPoint().distance(new Point(node.getX() + Node.radius, node.getY() + Node.radius)) <= Node.minNodeDist) {
+                    canPlace = false;
+                    break;
+                }
+
+            if (canPlace)
+                panel.getGraph().addNode(e.getX() - Node.radius,e.getY() - Node.radius);
+        }
+
+        isDragging = false;
+
+        panel.repaint();
 
     }
-
     @Override
     public void mouseClicked(MouseEvent e) {
 
     }
 
     @Override
-    public void mouseReleased(MouseEvent e) {
+    public void mouseDragged(MouseEvent e) {
 
     }
 
