16b765d11bbd22009e86467cc14e18dab1c2a7b7
Offuru <andrei.rozmarin@gmail.com>
2023-11-09 19:54:36 +0200

    Added utility methods for checking edge existance and adding and removing nodes and edges

--- GraphBuilder/src/GraphBuilder/models/Graph.java
+++ GraphBuilder/src/GraphBuilder/models/Graph.java
@@ -2,6 +2,7 @@ package GraphBuilder.models;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.awt.*;
 
 import GraphBuilder.Panel;
 
@@ -9,13 +10,15 @@ public class Graph {
 
     private List<Node> nodes;
     private List<List<Node>> adjancencyList;
-
+    private boolean directed;
     private List<Edge> edgeList;
 
     public Graph() {
 
         nodes = new ArrayList<>();
         adjancencyList = new ArrayList<>();
+        edgeList = new ArrayList<>();
+        directed = false;
     }
 
     public List<Node> getNodes() {
@@ -48,18 +51,32 @@ public class Graph {
     }
 
     public void addNode(int key, int x, int y) {
-        Node node = new Node(key, x, y);
-        addNode(node);
+        addNode(new Node(key, x, y));
+    }
+
+    public void addNode(int x, int y) {
+        addNode(new Node(nodes.size(), x, y));
     }
 
     public void addEdge(Edge edge) {
-        edgeList.add(edge);
-        adjancencyList.get(edge.getStart().getKey()).add(edge.getEnd());
+
+        if (!edgeList.contains(edge)) {
+            edgeList.add(edge);
+            adjancencyList.get(edge.getStart().getKey()).add(edge.getEnd());
+        }
+
+        if (!directed) {
+            Edge reverse = new Edge(edge.getEnd(), edge.getStart());
+
+            if (!edgeList.contains(reverse)) {
+                edgeList.add(reverse);
+                adjancencyList.get(reverse.getStart().getKey()).add(reverse.getEnd());
+            }
+        }
     }
 
     public void addEdge(Node node1, Node node2) {
-        Edge edge = new Edge(node1, node2);
-        addEdge(edge);
+        addEdge(new Edge(node1, node2));
     }
 
     public Node getNode(int key) {
@@ -68,6 +85,41 @@ public class Graph {
 
     public List<Node> neighbours(Node node) {
         return adjancencyList.get(node.getKey());
+    }
+
+    public boolean isEdge(Node node1, Node node2) {
+        return (adjancencyList.get(node1.getKey()).contains(node2) || adjancencyList.get(node2.getKey()).contains(node1));
+    }
+
+    public void removeNode(Node node) {
+
+        nodes.remove(node);
+
+        adjancencyList.remove(node.getKey());
+        for (List<Node> neighbours : adjancencyList)
+            for (Node curNode : neighbours)
+                if (curNode.getKey() > node.getKey())
+                    curNode.setKey(curNode.getKey() - 1);
+
+        edgeList.removeIf(edge -> (edge.getStart() == node || edge.getEnd() == node));
+    }
+
+    public void removeEdge(Edge edge) {
+
+        edgeList.remove(edge);
+        adjancencyList.get(edge.getStart().getKey()).remove(edge.getEnd());
+
+
+        if (!directed) {
+            Edge reverse = new Edge(edge.getEnd(), edge.getStart());
+
+            edgeList.remove(edge);
+            adjancencyList.get(edge.getStart().getKey()).remove(edge.getEnd());
+        }
+    }
+
+    public void removeEdge(Node start, Node end) {
+        removeEdge(new Edge(start, end));
     }
 
     public void printGraph() {
@@ -86,5 +138,25 @@ public class Graph {
 
         System.out.println();
         System.out.println();
+    }
+
+    public boolean isDirected() {
+        return directed;
+    }
+
+    public void draw(Graphics2D g2d) {
+
+        if (edgeList != null) {
+            g2d.setStroke(Edge.lineStroke);
+            for (Edge edge : edgeList)
+                edge.drawEdge(g2d, directed);
+        }
+
+        if (nodes != null) {
+            g2d.setStroke(Node.borderStroke);
+            for (Node node : nodes)
+                node.drawNode(g2d);
+        }
+
     }
 }
